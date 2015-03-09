5cdde19003c35ab58d7d9d25d0ed38de2bcc5ad2
sjostedt <msjoestedt@gmail.com>
2015-03-09 12:04:12 +0100

    NU SKA DEN FUNGERA

--- ARDUINO/accelero.m
+++ ARDUINO/accelero.m
@@ -2,13 +2,11 @@
 clear all
 clc
 close all
-%a = arduino('/dev/tty.usbmodem1411');
-% a.serial
-%a.pinMode('SDA', 'input')
-% i = 1;
-s1 = serial('/dev/tty.usbmodem1411', 'BaudRate', 9600);
+% a = arduino('/dev/tty.usbmodem1411');
+% % a.serial
+% a.pinMode('SDA', 'input')
+
+s1 = serial('/dev/tty.usbmodem1411', 'BaudRate',9600)
 fopen(s1)
-while i == 1
-    test = fscanf(s1)
-%     print test
-end
+fprintf(s1, 'SDA')
+out = fscanf(s1)
