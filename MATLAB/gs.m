f1b2a70c00733ef764511b6f70f462e6309698e4
sjostedt <msjoestedt@gmail.com>
2015-04-20 20:10:35 +0200

    uppdatesoft

--- MATLAB/gs.m
+++ MATLAB/gs.m
@@ -2,33 +2,33 @@ clc
 clear all
 close all
 %% Maxon 148877  DC
-max_voltage = 48;
-max_current = 400000;
-Kt = 60.3E-3; %Ska vara 3 ;PPPPP
-etaGear = 0.8;
-Gear = 5.2;
-alu_mass = 0.2;
-motor_mass = 0.5;
-motor_power = 0.2;
-arduino_mass = 0.05;
-extra = 0.2;
-M_s = 0.2;   % Svänghjul massa
-Mtot = alu_mass + motor_mass + motor_power + arduino_mass + extra + M_s  %Kub massa
-Ms = M_s;           % massa svänghjul [kg]
-%Mk = 1.2;           % massa kub [kg]
-l = 0.17;           % sidlängd [m]
-%Mtot = Ms+Mk;       % totalvikt [kg]
-g = 9.82;            % gravitation [kg*m/s^2]
-nf = 7580;          % no load speed [rpm]
-V = 48;             % nätspänning [V]
-K_emf = 1/158;          % spänningskonstant [V/rpm]
-eta = 0.9;          % verkningsgrad []
-Rm = 1.16;          % inre resistans [Ohm]
-rs = l/2-0.01;      % radie svänghjul [m]
-Ik = 0.015; %0.004;         % tröghetsmoment kub []
-Is = Ms*rs^2/2;     % tröghetsmoment svänghjul []
-L = 0.329E-3;        % Induktans motor [H]
-J = 138E-7;             % Inertia rotor
+max_voltage = 48;       %[V]
+max_current = 4;        %[A]       THIS IS THE PROBLEM
+Kt = 60.3E-3;           %[Nm/A]
+etaGear = 0.8;          %Gear efficiency
+Gear = 5.2;             %Gear ratio
+alu_mass = 0.2;         %Weight of the frame [Kg]
+motor_mass = 0.5;       %Weight of the motor [Kg]
+motor_power = 0.2;      %Weight of the motor control [Kg]
+arduino_mass = 0.05;    %Weight of the Arduino [Kg]
+extra = 0.2;            %Miscellanious weight [Kg]
+M_s = 0.2;              %Flywheel weight [Kg]
+Mtot = alu_mass + motor_mass + motor_power + arduino_mass + extra + M_s  %Total mass of cube [Kg]
+Ms = M_s;               % massa svänghjul [kg]
+l = 0.17;               %Length of the cube frame [m]
+g = 9.82;               %Gravitation [Kg*m/s^2]
+nf = 7580;              %No load speed [rpm]
+V = 48;                 %Nominal voltage [V]
+K_emf = 1/158;          %Voltage constant [V/rpm]
+eta = 0.9;              %Motor efficiency
+Rm = 1.16;              %Motor resistance [Ohm]
+rs = l/2-0.01;          %Radius flywheel [m]
+Ik = 0.015; %0.004;     %Inertia cube [kgm^2]
+Is = Ms*rs^2/2;         %Inertia flywheel [kgm^2]
+L = 0.329E-3;           %Motor inductance [H]
+J = 138E-7;             %Inertia rotor [kgm^2]
+
+%State space modelling
 % Assembly of A matrix
 a11 = 0;
 a12 = 1;
@@ -42,26 +42,9 @@ a31 = 0;
 a32 = Kt * Gear * etaGear * K_emf * eta / (Rm * Is);
 a33 = -Kt * Gear * etaGear * K_emf * eta / (Rm * Is);
 
-%%%%%%% Försök 2
-% a11 = 0;
-% a12 = 1;
-% a13 = 0;
-% 
-% a21 = -Mtot * g * l / (sqrt(2) * Ik);
-% a22 = 0;
-% a23 = K_emf * Kt * etaGear * eta / (Rm * Ik);
-% 
-% a31 = 0;
-% a32 = 0;
-% a33 = -Kt * etaGear * K_emf * eta / (Rm * Is);
-%%%%
 A = [a11, a12, a13; a21, a22, a23; a31, a32, a33];
 
 % Assembly of B matrix
-b1 = 0;
-b2 = -Kt * Gear * etaGear * eta / (Rm * Ik);
-b3 = Kt * Gear * etaGear * eta / (Rm * Is);
-%%%%% Försök 2
 b1 = 0;
 b2 = -Kt * Gear * etaGear * eta / (Rm * Ik);
 b3 = Kt * Gear * etaGear * eta / (Rm * Is);
@@ -102,29 +85,29 @@ K = place(A, B, [p0, p1, p2]);
 % R = 3;
 % [K] = lqr(A,B,Q,R)
 
-sys_cl = ss(A-B*K,B,C,D, 'statename',states,'inputname',inputs,'outputname',outputs) 
-lsim(sys_cl,U,t,x0)
+% sys_cl = ss(A-B*K,B,C,D, 'statename',states,'inputname',inputs,'outputname',outputs) 
+% lsim(sys_cl,U,t,x0)
+% 
+% % Control Theory
+% poles = eig(A)
+% sys = ss(A,B,C,D)
+% rank(ctrb(sys))
+% min_current = Mtot*g*l*sin(theta_start*pi/180)/(sqrt(2)*Kt*eta*etaGear*Gear)
 
-% Control Theory
-poles = eig(A)
-sys = ss(A,B,C,D)
-rank(ctrb(sys))
-min_current = Mtot*g*l*sin(theta_start*pi/180)/(sqrt(2)*Kt*eta*etaGear*Gear)
 
-
-%% Model validation
-close all
-Motorval = figure
-time = 5;
-t=0:time/length(vel):time-time/length(vel);
-subplot(1,2,1)
-plot (t, vel)
-title('Motor speed')
-xlabel('Time [s]')
-ylabel('Speed [rpm]')
-hold on
-subplot(1,2,2)
-plot(t, Current)
-title('Motor current')
-xlabel('Time [s]')
-ylabel('Current [A]')
+% %% Model validation
+% close all
+% Motorval = figure
+% time = 5;
+% t=0:time/length(vel):time-time/length(vel);
+% subplot(1,2,1)
+% plot (t, vel)
+% title('Motor speed')
+% xlabel('Time [s]')
+% ylabel('Speed [rpm]')
+% hold on
+% subplot(1,2,2)
+% plot(t, Current)
+% title('Motor current')
+% xlabel('Time [s]')
+% ylabel('Current [A]')
